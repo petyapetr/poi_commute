19c93d67417f904fefdd03bba8a9d3626555cc1e
Slava Zakharov <slava.zakharov@mailbox.org>
2023-09-15 19:13:57 +0400

    Add layers and filter for POIs

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # poi_commute
 Web app with a 3d map of POIs and nearby bus stations 
+
+**IMPORTANT**
+To view it locally disable CORS policy in your browser.
---
+++ main.js
@@ -0,0 +1,155 @@
+import esriConfig from "https://js.arcgis.com/4.27/@arcgis/core/views/MapView.js";
+import Map from "https://js.arcgis.com/4.27/@arcgis/core/Map.js";
+import SceneView from "https://js.arcgis.com/4.27/@arcgis/core/views/SceneView.js";
+import FeatureLayer from "https://js.arcgis.com/4.27/@arcgis/core/layers/FeatureLayer.js";
+import SceneLayer from "https://js.arcgis.com/4.27/@arcgis/core/layers/SceneLayer.js";
+import Expand from "https://js.arcgis.com/4.27/@arcgis/core/widgets/Expand.js";
+
+// Initiallization
+esriConfig.apiKey =
+	"AAPK5768bdfdf6934d3f9e993b500647accdENViXgjJamoSamjjq4X1Orw00pCFHyN3scHiqRbw5vblcXzfL4WuLL6O06X9MnwF";
+
+const map = new Map({
+	basemap: "osm-streets-relief",
+	ground: "world-elevation",
+});
+const view = new SceneView({
+	container: "viewDiv",
+	map: map,
+	camera: {
+		position: {
+			x: 44.512943, //Longitude
+			y: 40.167916, //Latitude
+			z: 1400, //Meters
+		},
+		tilt: 75,
+	},
+});
+
+const buildingsLayer = new SceneLayer({
+	url: "https://basemaps3d.arcgis.com/arcgis/rest/services/OpenStreetMap3D_Buildings_v1/SceneServer",
+	popupEnabled: false,
+	mouseEventsEnabled: false,
+});
+map.add(buildingsLayer);
+
+// Definning Popups
+const poiPopup = {
+	title: "{name}",
+	content: `<b>Category:</b> {category_name}<br><b>Address:</b> {address_name}<br><b>Coordinates:</b> {latitude} N, {longitude} E<br>`,
+};
+view.popup = {
+	dockEnabled: true,
+	dockOptions: {
+		buttonEnabled: false,
+		breakpoint: false,
+		position: "bottom-left",
+	},
+};
+
+const poiRenderer = {
+	type: "unique-value",
+	field: "category_name",
+	defaultSymbol: {
+		type: "point-3d",
+		symbolLayers: [
+			{
+				type: "icon", // autocasts as new IconSymbol3DLayer()
+				resource: {
+					href: "https://developers.arcgis.com/javascript/latest/sample-code/visualization-point-styles/live/Museum.png",
+				},
+				size: 20,
+				outline: {
+					color: "white",
+					size: 2,
+				},
+			},
+		],
+
+		verticalOffset: {
+			screenLength: 40,
+			maxWorldLength: 200,
+			minWorldLength: 35,
+		},
+
+		callout: {
+			type: "line", // autocasts as new LineCallout3D()
+			color: "white",
+			size: 2,
+			border: {
+				color: "#D13470",
+			},
+		},
+	},
+};
+
+const poiLayer = new FeatureLayer({
+	url: "https://services4.arcgis.com/XZEtqni2CM1tP1ZM/ArcGIS/rest/services/YerevanPOIs/FeatureServer/1",
+	outFields: ["name", "category_name", "address_name", "telephone", "latitude", "longitude"],
+	popupTemplate: poiPopup,
+	// renderer: poiRenderer,
+});
+const busStopsLayer = new FeatureLayer({
+	url: "https://services4.arcgis.com/XZEtqni2CM1tP1ZM/arcgis/rest/services/Bus_Service_WFL1/FeatureServer/1",
+});
+
+map.add(poiLayer);
+map.add(busStopsLayer);
+
+// set widget and renderer when layer is loaded
+view.whenLayerView(poiLayer).then(
+	(layerView) => {
+		const poiLayerView = layerView
+		console.log("success");
+		// Define categories and create a widget
+		const categories = poiLayer.renderer.uniqueValueGroups[0].classes.map((val) => val.label);
+		// const filterNode = document.getElementById("filter-widget-container");
+		const filterNode = document.createElement("div");
+		filterNode.classList.add("filter-widget-container");
+		const filterExpand = setupFilterWidget(categories, filterNode);
+		view.ui.add(filterExpand, "top-right");
+		// filterExpand.watch("expanded");
+
+		// create filter watcher
+		// click event handler for seasons choices
+		filterNode.addEventListener("click", filterByCategory);
+
+		function filterByCategory(event) {
+			const selectedCategory = event.target.getAttribute("category-data");
+
+			console.log(selectedCategory);
+			console.log(selectedCategory);
+			poiLayerView.filter = {
+				where: `category_name = '${selectedCategory}'`,
+			};
+		}
+
+		// setup a poi renderer style
+		// const colors = poiLayer.renderer.
+		poiLayer.renderer = poiRenderer;
+	},
+	(err) => console.log(err)
+);
+
+function setupFilterWidget(categories, node) {
+	categories.forEach((name) => {
+		const childNode = document.createElement("div");
+
+		childNode.textContent = name;
+		childNode.classList.add("category-item");
+		childNode.setAttribute("category-data", name);
+
+		node.appendChild(childNode);
+	});
+
+	const filterWidget = new Expand({
+		content: node,
+		view: view,
+		expandIcon: "filter",
+		id: "filter_widget",
+	});
+
+	return filterWidget;
+}
+
+
